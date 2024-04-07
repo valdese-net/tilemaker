8cad97b813ed00403bb9d82599aec2845fb45e9d
nwsw <online@noteworthysoftware.com>
2024-04-07 12:53:31 -0400

    Simplify VLP pmtiles

--- runit.sh
+++ runit.sh
@@ -4,18 +4,26 @@ mkdir -p data
 # fetch the latest for North Carolina https://download.geofabrik.de/north-america/us/north-carolina-latest.osm.pbf
 wget https://download.geofabrik.de/north-america/us/north-carolina-latest.osm.pbf -q --show-progress -N -P ./data
 
+# terrain USGS 1/3 Arc Second
+# https://github.com/nst-guide/terrain
+# wget https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/13/TIFF/historical/n36w082/USGS_13_n36w082_20220512.tif  -q --show-progress -N -P ./data
+
 # extract Burke and Valdese areas
 # prereq: sudo apt install osmctools
 # --complex-ways --strategy=smart yielded fragments
 # --complete-ways --complete-multipolygons --complete-boundaries
 osmconvert data/north-carolina-latest.osm.pbf -b=-82,35.55,-81.35,36.01 --complete-boundaries -o=data/burke.osm.pbf
 osmconvert data/north-carolina-latest.osm.pbf -b=-81.623,35.725,-81.523,35.790 --complete-boundaries -o=data/valdese.osm.pbf
+# alternate: -81.57286,35.75574 -81.53452,35.77947
 osmconvert data/north-carolina-latest.osm.pbf -b=-81.5781,35.7619,-81.5293,35.7883 --complete-boundaries -o=data/vlp.osm.pbf
 
 docker run -v ./:/srv -i -t --rm tilemaker /srv/data/north-carolina-latest.osm.pbf --output=/srv/data/nc.pmtiles --config /srv/tilemaker-allpaths.json --process /srv/tilemaker-allpaths.lua
 docker run -v ./:/srv -i -t --rm tilemaker /srv/data/burke.osm.pbf --output=/srv/data/burke.pmtiles --config /srv/tilemaker-allpaths.json --process /srv/tilemaker-allpaths.lua
 docker run -v ./:/srv -i -t --rm tilemaker /srv/data/valdese.osm.pbf --output=/srv/data/valdese.pmtiles --config /srv/tilemaker-allpaths.json --process /srv/tilemaker-allpaths.lua
 docker run -v ./:/srv -i -t --rm tilemaker /srv/data/vlp.osm.pbf --output=/srv/data/vlp.pmtiles --config /srv/tilemaker-allpaths.json --process /srv/tilemaker-allpaths.lua
+
+# valdese-area generated via https://app.protomaps.com/
+docker run -v ./:/srv -i -t --rm tilemaker /srv/data/valdese-area.osm.pbf --output=/srv/data/valdese-area.pmtiles --config /srv/tilemaker-vlp.json --process /srv/tilemaker-vlp.lua
 
 #pmtiles convert valdese.mbtiles valdese.pmtiles
 #pmtiles convert burke.mbtiles burke.pmtiles
--- tilemaker-vlp.lua
+++ tilemaker-vlp.lua
@@ -17,17 +17,6 @@ function Set(list)
 	return set
 end
 
--- Meters per pixel if tile is 256x256
-ZRES5  = 4891.97
-ZRES6  = 2445.98
-ZRES7  = 1222.99
-ZRES8  = 611.5
-ZRES9  = 305.7
-ZRES10 = 152.9
-ZRES11 = 76.4
-ZRES12 = 38.2
-ZRES13 = 19.1
-
 -- The height of one floor, in meters
 BUILDING_FLOOR_HEIGHT = 3.66
 
@@ -68,14 +57,7 @@ function way_function()
 	local landuse  = Find("landuse")
 	local leisure  = Find("leisure")
 
-	if leisure=="park" and Holds("name") then
-		Layer("park", true)
-		Attribute("class", leisure)
-		MinZoom(GetMinZoomByArea())
-	end
-
-	-- Roads
-	if highway~="" then
+	if highway~="" and (Holds("name") or Holds("ref")) then
 		local _,_,linked_path = highway:find("^(%l+)_link")
 		if linked_path then
 			highway = linked_path
@@ -84,57 +66,34 @@ function way_function()
 			highway = "path"
 		end
 
-		local minzoom = 99
 		local objtype = "road"
 		local objclass = highway
 
-		if majorRoadValues[highway] then minzoom = 4 end
-		if highway == "trunk" then minzoom = 5
-		elseif highway == "primary" then minzoom = 7 end
-		if mainRoadValues[highway] then minzoom = 9 end
-		if midRoadValues[highway] then minzoom = 11 end
-		if minorRoadValues[highway] then minzoom = 12 end
 		if pathValues[highway] then
-			minzoom = 14
 			objtype = "path"
 		elseif trackValues[highway] then
-			minzoom = 14
 			objtype = "path"
 		end
 
-		if minzoom <= 14 then
+		if objtype ~= "path" then
 			Layer(objtype, false)
 			--if highway=="unclassified" or highway=="residential" then highway="minor" end
 			Attribute("class", objclass)
-			MinZoom(minzoom)
 			if linked_path then AttributeNumeric("ramp",1) end
 
 			local name = getAsciiName()
 			if name then
-				Layer("label", false)
-				Attribute("class", objtype)
-				Attribute("subclass", objclass)
-				MinZoom(math.min(minzoom+1,14))
 				Attribute("name", name)
 			end
 
 			if roadsWithRef[objclass] and Holds("ref") and not linked_path then
 				local ref = Find("ref")
-				Layer("label", false)
 				Attribute("ref", ref)
-				Attribute("class", "shield")
-				Attribute("subclass", objclass)
-				MinZoom(13)
 			end
-		end
-	end
-
-	-- Lakes and other water polygons
-	if Find("natural")=="water" then
+		end			
+	elseif Find("natural")=="water" then
 		local c = (Find("water")=="river") and "river" or "lake"
 		Layer("water", true)
-		local minzoom = GetMinZoomByArea()
-		MinZoom(minzoom)
 		Attribute("class", c)
 		local name = getAsciiName()
 		if name then
@@ -146,35 +105,24 @@ function way_function()
 			
 			Attribute("class", "water")
 			Attribute("subclass", c)
-			MinZoom(minzoom)
 			Attribute("name", name)
 		end
-	end
-
-	-- Rivers
-	if waterway=="stream" or waterway=="river" or waterway=="canal" then
+	elseif waterway=="stream" or waterway=="river" or waterway=="canal" then
 		Layer("waterway", false)
 		if Find("intermittent")=="yes" then AttributeNumeric("intermittent", 1) else AttributeNumeric("intermittent", 0) end
 		Attribute("class", waterway)
-		MinZoom(11)
 		local name = getAsciiName()
 		if name then
-			Layer("label", false)
-			Attribute("class", "waterway")
-			Attribute("subclass", waterway)
-			MinZoom(13)
 			Attribute("name", name)
 		end
-	end
-
-	-- Buildings
-	if showBuildings[building] then
+	elseif leisure=="park" and Holds("name") then
+		Layer("park", true)
+		Attribute("class", leisure)
+	elseif showBuildings[building] then
 		Layer("building", true)
 		Attribute("class", building)
 		SetBuildingHeightAttributes()
-		MinZoom(GetMinZoomByArea())
 	end
-
 end
 
 function isAscii(s)
@@ -213,18 +161,4 @@ function SetBuildingHeightAttributes()
 
 	AttributeNumeric("render_height", renderHeight)
 	AttributeNumeric("render_min_height", renderMinHeight)
-end
-
--- Set minimum zoom level by area
-function GetMinZoomByArea()
-	local area=Area()
-	if area>ZRES5^2  then return 6 end
-	if area>ZRES6^2  then return 7 end
-	if area>ZRES7^2  then return 8 end
-	if area>ZRES8^2  then return 9 end
-	if area>ZRES9^2  then return 10 end
-	if area>ZRES10^2 then return 11 end
-	if area>ZRES11^2 then return 12 end
-	if area>ZRES12^2 then return 13 end
-	return 14
 end
